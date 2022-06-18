297d374d0682646a6f645f0b869f26f1953c0fd1
SultanEsen <beksultan.esenkulov@gmail.com>
2022-06-18 15:21:26 +0600

    First commit

---
+++ hw2.py
@@ -0,0 +1,35 @@
+import time
+
+
+class Company:
+    def __init__(self, company_name, company_bank):
+        self.company_name = company_name
+        self.company_bank = company_bank
+
+
+class Person(Company):
+    def __init__(self, company_name, company_bank, first_name, last_name, salary):
+        super().__init__(company_name, company_bank)
+        self.first_name = first_name
+        self.last_name = last_name
+        self.salary = salary
+
+    def get_salary(self):
+        if self.salary > self.company_bank:
+            print(f'The {self.company_name} company does not have enough funds!”')
+        else:
+            self.company_bank -= self.salary
+            print(f'{self.first_name} {self.last_name} received salary of {self.salary} USD\n'
+                  f'Current balance of {self.company_name}: {self.company_bank}\n***')
+
+    def person_info(self):
+        print(f'First name: {self.first_name}\nLast name: {self.last_name}\nSalary: {self.salary} USD\n')
+
+
+person1 = Person('IBM', 30000, 'Tom', 'Hanks', 10000)
+person1.person_info()
+c = 0
+while c != 5:
+    person1.get_salary()
+    c += 1
+    time.sleep(1)
